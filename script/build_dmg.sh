05e6454cd6f54cd367f51a50860d19790e8c71c2
Brian Xie <yagoodidea@gmail.com>
2026-09-10 20:15:09 +0800

    chore: remove Install.txt from DMG package

--- script/build_dmg.sh
+++ script/build_dmg.sh
@@ -23,24 +23,6 @@ trap 'rm -rf "$STAGING_DIR"' EXIT
 mkdir "$STAGING_DIR/contents"
 ditto "$APP_BUNDLE" "$STAGING_DIR/contents/CapsLock Bye.app"
 ln -s /Applications "$STAGING_DIR/contents/Applications"
-cat > "$STAGING_DIR/contents/Install.txt" <<'NOTES'
-CapsLock Bye
-macOS 14+ · Apple Silicon and Intel
-
-Installation
-1. Drag CapsLock Bye to Applications, then eject this disk image.
-2. Open CapsLock Bye from Applications.
-3. Follow the app's guide to enable Accessibility and Input Monitoring.
-4. Reopen the app if macOS asks, then enable mapping and try the key tester.
-
-安装
-1. 将 CapsLock Bye 拖到 Applications（应用程序），然后推出磁盘映像。
-2. 从“应用程序”打开 CapsLock Bye。
-3. 按应用内引导开启“辅助功能”和“输入监控”权限。
-4. 如果 macOS 要求，重新打开应用，再开启映射并使用试键区验证。
-
-https://github.com/nixihz/capslock-bye
-NOTES
 
 hdiutil create -volname "CapsLock Bye" -srcfolder "$STAGING_DIR/contents" \
     -format UDZO -fs HFS+ "$STAGING_DIR/$DMG_NAME"
